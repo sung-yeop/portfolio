06f20c5689f37cdb3cf93256326a431493276b46
sung-yeop <jjabc9459@naver.com>
2025-07-02 22:02:04 +0900

    test: Lint staged 테스트

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,11 +1,15 @@
-import { ReactNode } from "react"
-import { ReactQueryProvider } from "./ReactQueryProvider"
-import { MSWProvider } from "./MswProvider"
+import { ReactNode } from 'react'
+import { ReactQueryProvider } from './ReactQueryProvider'
+import { MSWProvider } from './MswProvider'
 
 type Props = {
-    children : ReactNode
+  children: ReactNode
 }
 
-export default function Context({children} : Props) {
-return <ReactQueryProvider><MSWProvider>{children}</MSWProvider></ReactQueryProvider>
+export default function Context({ children }: Props) {
+  return (
+    <ReactQueryProvider>
+      <MSWProvider>{children}</MSWProvider>
+    </ReactQueryProvider>
+  )
 }
